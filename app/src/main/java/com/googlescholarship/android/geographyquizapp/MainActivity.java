e831e5e2c70401906bce5506e586478ca7a3819d
dorinmoldovan <12948353+dorinmoldovan@users.noreply.github.com>
2018-02-04 00:08:55 +0200

    add new questions

--- app/src/main/java/com/googlescholarship/android/geographyquizapp/MainActivity.java
+++ app/src/main/java/com/googlescholarship/android/geographyquizapp/MainActivity.java
@@ -4,11 +4,14 @@ import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.CheckBox;
+import android.widget.EditText;
 import android.widget.RadioButton;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    public static final int MAX_SCORE = 5;
     private int finalScore = 0;
 
     @Override
@@ -17,19 +20,55 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
+    /**
+     * This method computes the final score of the Geography Quiz App and displays the result in a
+     * toast.
+     *
+     * @param v
+     */
     public void computeScore(View v) {
         finalScore = 0;
-
-        RadioButton bucharestRadioButton = (RadioButton) findViewById(R.id.bucharest_radio_button);
-        if(bucharestRadioButton.isChecked()) {
-            finalScore++;
-        }
-
+        computeScoreForTheFirstQuestion();
+        computeScoreForTheSecondQuestion();
+        computeScoreForTheThirdQuestion();
         Context context = getApplicationContext();
-        CharSequence text = getString(R.string.score_message) + finalScore;
+        CharSequence text = getString(R.string.score_message_start) + finalScore
+                + getString(R.string.score_message_end) + MAX_SCORE;
         int duration = Toast.LENGTH_SHORT;
-
         Toast toast = Toast.makeText(context, text, duration);
         toast.show();
     }
+
+    /**
+     * This method computes the score for the first question.
+     */
+    private void computeScoreForTheFirstQuestion() {
+        RadioButton bucharestRadioButton = (RadioButton) findViewById(R.id.bucharest_radio_button);
+        if (bucharestRadioButton.isChecked()) {
+            finalScore += 1;
+        }
+    }
+
+    /**
+     * This method computes the score for the second question.
+     */
+    private void computeScoreForTheSecondQuestion() {
+        CheckBox franceCheckBox = (CheckBox) findViewById(R.id.france_checkbox);
+        CheckBox germanyCheckBox = (CheckBox) findViewById(R.id.germany_checkbox);
+        CheckBox russiaCheckBox = (CheckBox) findViewById(R.id.russia_checkbox);
+        if (franceCheckBox.isChecked() && germanyCheckBox.isChecked() && !russiaCheckBox.isChecked()) {
+            finalScore += 1;
+        }
+    }
+
+    /**
+     * This method computes the score for the third question.
+     */
+    private void computeScoreForTheThirdQuestion() {
+        EditText editText = (EditText) findViewById(R.id.largest_continent);
+        if(editText.getText().toString().equals(getString(R.string.asia))) {
+            finalScore += 1;
+        }
+    }
+
 }
