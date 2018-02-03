0a660aa4d28683e0abf145a3ff92ed56aa3bd876
dorinmoldovan <12948353+dorinmoldovan@users.noreply.github.com>
2018-02-03 23:00:23 +0200

    first question

--- app/src/main/java/com/googlescholarship/android/geographyquizapp/MainActivity.java
+++ app/src/main/java/com/googlescholarship/android/geographyquizapp/MainActivity.java
@@ -1,13 +1,35 @@
 package com.googlescholarship.android.geographyquizapp;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.RadioButton;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private int finalScore = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void computeScore(View v) {
+        finalScore = 0;
+
+        RadioButton bucharestRadioButton = (RadioButton) findViewById(R.id.bucharest_radio_button);
+        if(bucharestRadioButton.isChecked()) {
+            finalScore++;
+        }
+
+        Context context = getApplicationContext();
+        CharSequence text = getString(R.string.score_message) + finalScore;
+        int duration = Toast.LENGTH_SHORT;
+
+        Toast toast = Toast.makeText(context, text, duration);
+        toast.show();
     }
 }
